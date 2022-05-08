6baf350b7e806c9cc11bd5af8d2b52f049e46bbe
geb <853934146@qq.com>
2022-05-09 01:11:58 +0800

    update

--- umr/a3c/agent.py
+++ umr/a3c/agent.py
@@ -9,7 +9,7 @@ class AgentWorker(Process):
 
     def __int__(self, env_name: str, pid: int, url_c2s: str, url_s2c: str):
         super(AgentWorker, self).__init__()
-        self.env = get_gym_env(env_name)
+        self.env = self.__get_env(env_name)
         self.id = int(pid)
         self.name = f'worker-{self.id}'
         self.identity = self.name.encode('utf-8')
@@ -26,7 +26,6 @@ class AgentWorker(Process):
         self.s2c_socket.connect(url_s2c)
 
     def run(self):
-
         state = self.env.reset()
         reward, done = 0, False
         while True:
@@ -35,6 +34,9 @@ class AgentWorker(Process):
             state, reward, done, _ = self.env.step(action)
             if done:
                 state = self.env.reset()
+
+    def __get_env(self, env_name: str) -> gym.Env:
+        return get_gym_env(env_name)
 
 
 class AgentMaster(Thread):
