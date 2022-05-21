48aee8f6fa29703c8f9e60f9a33685fbce12f804
geb <853934146@qq.com>
2022-05-21 15:36:27 +0800

    update

--- umr/a3c/play.py
+++ umr/a3c/play.py
@@ -5,23 +5,31 @@ import numpy as np
 
 env_name = "Breakout-v5"
 
-env = get_gym_env(f"ALE/{env_name}", render_mode="human", is_train=False)
+env = get_gym_env(f"ALE/{env_name}", render_mode=None, is_train=False)
 
 model = A3C(env.action_space.n)
 latest = tf.train.latest_checkpoint(f'./train_log/train-ALE/{env_name}')
 model.load_weights(latest)
 observation = env.reset()
 score = 0
-while True:
-    distrib, _ = model(observation[np.newaxis, :])
-    if np.random.random(1)[0] < 0.01:
-        action = np.random.choice(env.action_space.n, p=distrib.numpy()[0])
-    else:
-        action = np.argmax(distrib.numpy()[0])
-    observation, reward, done, info = env.step(action)
-    score += reward
-    if done:
-        print(score)
-        observation = env.reset()
-        score = 0
+scores = []
+for _ in range(50):
+    while True:
+        logits, _ = model(observation[np.newaxis, :])
+        distrib = tf.nn.softmax(logits)
+        if np.random.random(1)[0] < 0.01:
+            action = np.random.choice(env.action_space.n, p=distrib.numpy()[0])
+        else:
+            action = np.argmax(distrib.numpy()[0])
+        observation, reward, done, info = env.step(action)
+        # env.render("human")
+        score += reward
+        if done:
+            print(score)
+            scores.append(score)
+            observation = env.reset()
+            score = 0
+            break
 env.close()
+
+print(np.mean(scores))
